c2326720667b23a20e4c20a126a0494b482579b7
Angel <angeljpa95@gmail.com>
2021-04-03 18:49:32 -0600

    Comenzando con basicos

---
+++ 01ModulosCore/00-basics.js
@@ -0,0 +1,26 @@
+/* 
+Organizacion del Codigo JS
+    - Librerias/modulos
+    - Constantes
+    - Objetos/variables
+    - funciones
+    - eventos
+    - ejecuciones
+
+UpperCamelCase
+    - Prototipos
+    - Clases
+lowerCamelCase
+    - Variables
+    - Metodos
+*/
+
+'use strict'
+
+console.log('Hola Mundo\n');
+console.log(4 + 9);
+console.log(global);
+
+setInterval(function (){
+    console.log('Hola node js')
+},1000)
